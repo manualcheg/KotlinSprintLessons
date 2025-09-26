032d4a348c23634f35f57b8cf4258a21381239df
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-26 14:41:59 +0300

    KS-1-1 Variables
    Cleared any garbage.
    Created 2 variables.

--- src/main/kotlin/lesson1/Lesson1.kt
+++
@@ -1,7 +0,0 @@
-package org.example.lesson1
-
-fun main(){
-    val str = "Kotlin"
-    println("hello "+ str+ "!")
-    println("Изменение_1_")
-}
---
+++ src/main/kotlin/lesson1/Lesson1_task1.kt
@@ -0,0 +1,6 @@
+package org.example.lesson1
+
+fun main() {
+    var countOfOrders: Short = 75
+    val thankYouText: String = "Благодарим за заказ!"
+}
