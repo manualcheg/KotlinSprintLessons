8e4602c2b601a8a50a6f42294f074f8770ac97bb
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-27 22:53:49 +0300

    KS-1-2 Variables stuff

--- src/main/kotlin/lesson1/Lesson1_task2.kt
+++ src/main/kotlin/lesson1/Lesson1_task2.kt
@@ -3,6 +3,8 @@ package org.example.lesson1
 fun main() {
     val countOfOrders: Short = 75
     val thankYouText: String = "Благодарим за заказ!"
+    val numberOfStuff: Short = 2000
     println(countOfOrders)
     println(thankYouText)
+    println(numberOfStuff)
 }
