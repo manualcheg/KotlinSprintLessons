aa3b3fb87ef8d17c26890bfcdc5561c4fac3402e
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-27 23:16:13 +0300

    KS-1-3 Variables flight-up time print

--- src/main/kotlin/lesson1/Lesson1_task3.kt
+++ src/main/kotlin/lesson1/Lesson1_task3.kt
@@ -2,4 +2,9 @@ package org.example.lesson1
 
 fun main(){
     val year: Short = 1961
+    val hour: Byte = 2
+    val minute: Byte = 10
+    println(year)
+    println(hour)
+    println(minute)
 }
