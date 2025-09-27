b9179dda8876ff88a380885b00e1be0655b51e55
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-27 22:52:40 +0300

    KS-1-2 Variables print variables

---
+++ src/main/kotlin/lesson1/Lesson1_task2.kt
@@ -0,0 +1,8 @@
+package org.example.lesson1
+
+fun main() {
+    val countOfOrders: Short = 75
+    val thankYouText: String = "Благодарим за заказ!"
+    println(countOfOrders)
+    println(thankYouText)
+}
