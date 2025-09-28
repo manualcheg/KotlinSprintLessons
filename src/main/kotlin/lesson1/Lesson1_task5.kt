214451d954f30bf066176e9784897a24f1bba8ed
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-28 16:40:18 +0300

    KS-1-5 Variables refactoring 2

--- src/main/kotlin/lesson1/Lesson1_task5.kt
+++ src/main/kotlin/lesson1/Lesson1_task5.kt
@@ -3,9 +3,9 @@ package org.example.lesson1
 const val TIME_CONST = 60
 
 fun main() {
-    var seconds: Int = 6480
-    var minutes: Int = seconds / TIME_CONST
-    var hours: Int = minutes / TIME_CONST
+    val seconds: Int = 6480
+    val minutes: Int = seconds / TIME_CONST
+    val hours: Int = minutes / TIME_CONST
     val secondsRemaining = seconds % TIME_CONST
     val minutesRemaining = minutes % TIME_CONST
 
