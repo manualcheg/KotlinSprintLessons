37bb10206a10662418872c42eb144a99aad702ed
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-29 18:53:53 +0300

    KS-2-3 Math operation Done

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,5 +1,17 @@
 package org.example.lesson2
 
-fun main() {
+import org.example.lesson1.TIME_CONST
 
+fun main() {
+    val hourStart: Int = 9
+    val minutesStart: Int = 39
+    val timeTravel: Int = 457
+    var hourArrive: Int = hourStart + timeTravel / TIME_CONST
+    val minutesArrive: Int = if (minutesStart + timeTravel % TIME_CONST < TIME_CONST) {
+        minutesStart + timeTravel % TIME_CONST
+    } else {
+        hourArrive++
+        minutesStart + timeTravel % TIME_CONST - TIME_CONST
+    }
+    println("Время прибытия поезда: $hourArrive:$minutesArrive")
 }
