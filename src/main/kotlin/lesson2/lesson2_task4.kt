cdd7983b71dc9baabd81d1d77b5ef67103183037
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-29 21:34:32 +0300

    KS-2-4 Math operation refactoring

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,12 +1,13 @@
 package org.example.lesson2
 
-const val BUFF: Float = 0.2F
+const val BUFF = 20
 
 fun main() {
-    val quantityIron = 7
-    val quantityCrystal = 11
-    val bonusIronWithBuff = (quantityIron*BUFF).toInt()
-    val bonusCrystalWithBuff = (quantityCrystal*BUFF).toInt()
+    val quantityIron = 11
+    val quantityCrystal = 7
+    val percent: Float = BUFF / 100F
+    val bonusIronWithBuff = (quantityIron * percent).toInt()
+    val bonusCrystalWithBuff = (quantityCrystal * percent).toInt()
     println("Бонус Железной руды: $bonusIronWithBuff")
     println("Бонус Кристаллической руды: $bonusCrystalWithBuff")
 }
