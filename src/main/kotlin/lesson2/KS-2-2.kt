764902be7e70a1d6a4bd29be1155ce77c7cc43cc
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-29 21:06:14 +0300

    KS-2-2 Math operation refactoring

--- src/main/kotlin/lesson2/KS-2-2.kt
+++ src/main/kotlin/lesson2/KS-2-2.kt
@@ -1,15 +1,16 @@
 package org.example.lesson2
 
-const val TEAM_SIZE = 50
-const val SALARY_TEAM_WORKER = 30_000
-const val INTERNS_SIZE = 30
-const val SALARY_INTERN = 20_000
 
 fun main() {
-    val salaryCostsTeam = TEAM_SIZE * SALARY_TEAM_WORKER
-    val salaryCostsInterns = INTERNS_SIZE * SALARY_INTERN
+    val teamSize = 50
+    val salaryTeamWorker = 30_000
+    val internsSize = 30
+    val salaryIntern = 20_000
+    val salaryCostsTeam = teamSize * salaryTeamWorker
+    val salaryCostsInterns = internsSize * salaryIntern
     val summarySalaryCosts = salaryCostsTeam + salaryCostsInterns
-    val middleSalary = (salaryCostsTeam + salaryCostsInterns) / (TEAM_SIZE + INTERNS_SIZE)
+    val middleSalary = summarySalaryCosts / (teamSize + internsSize)
+
     println("Расходы на выплату зарплаты постоянных сотрудников: \n$salaryCostsTeam рублей")
     println("Общие расходы по ЗП после прихода стажеров: \n$summarySalaryCosts рублей")
     println("Средняя ЗП одного сотрудника после устройства стажеров: \n$middleSalary рублей")
