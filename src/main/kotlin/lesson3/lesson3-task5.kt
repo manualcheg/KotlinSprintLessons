ec39d885ce88e9cdecad015cf9dfbcf1e36e7fc0
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-30 23:48:20 +0300

    KS-3-5 chess split refactoring

--- src/main/kotlin/lesson3/lesson3-task5.kt
+++ src/main/kotlin/lesson3/lesson3-task5.kt
@@ -2,7 +2,7 @@ package org.example.lesson3
 
 fun main() {
     val move = "D2-D4;0"
-    val moveArray = move.split(",", ";", "-")
+    val moveArray = move.split(";", "-")
     println(moveArray[0])
     println(moveArray[1])
     println(moveArray[2])
