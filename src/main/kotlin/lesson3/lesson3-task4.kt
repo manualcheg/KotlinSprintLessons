e2309431e7b40bf96549932e1e7b930768d66119
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-30 15:34:55 +0300

    KS-3-4 done

---
+++ src/main/kotlin/lesson3/lesson3-task4.kt
@@ -0,0 +1,12 @@
+package org.example.lesson3
+
+fun main() {
+    var from = "E2"
+    var where = "E4"
+    var countOfMove = 1
+    println("[$from-$where;$countOfMove]")
+    from = "G2"
+    where = "G4"
+    countOfMove ++
+    println("[$from-$where;$countOfMove]")
+}
