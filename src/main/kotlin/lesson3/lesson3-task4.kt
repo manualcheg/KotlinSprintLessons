3da68e9756c5cbe319109c81a58720f7984f76b1
KochubeevAndrei <manualcheg@yandex.ru>
2025-09-30 23:44:53 +0300

    KS-3-4 chess refactoring

--- src/main/kotlin/lesson3/lesson3-task4.kt
+++ src/main/kotlin/lesson3/lesson3-task4.kt
@@ -5,8 +5,9 @@ fun main() {
     var where = "E4"
     var countOfMove = 1
     println("[$from-$where;$countOfMove]")
-    from = "G2"
-    where = "G4"
-    countOfMove ++
+
+    from = "D2"
+    where = "D3"
+    countOfMove++
     println("[$from-$where;$countOfMove]")
 }
