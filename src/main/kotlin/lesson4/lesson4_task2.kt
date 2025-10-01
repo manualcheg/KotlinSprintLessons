a599f8cffa0623bb32f559fc8af0d39ddacc5bb2
KochubeevAndrei <manualcheg@yandex.ru>
2025-10-01 15:37:49 +0300

    KS-4-2 Check weight category Average

---
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson4
+
+fun main() {
+    var weight = 20
+    var volume = 80
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${weight > 35 && weight <= 100 && volume < 100}"
+    )
+    weight = 50
+    volume = 100
+    println(
+        "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
+                "${weight > 35 && weight <= 100 && volume < 100}"
+    )
+}
