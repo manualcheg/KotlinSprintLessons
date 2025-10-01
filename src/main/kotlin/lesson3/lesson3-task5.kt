55b16d459399059834ecc01dd28fdb6d39762218
KochubeevAndrei <manualcheg@yandex.ru>
2025-10-01 15:40:35 +0300

    main dont needed files

--- src/main/kotlin/lesson3/lesson3-task5.kt
+++
@@ -1,9 +0,0 @@
-package org.example.lesson3
-
-fun main() {
-    val move = "D2-D4;0"
-    val moveArray = move.split(";", "-")
-    println(moveArray[0])
-    println(moveArray[1])
-    println(moveArray[2])
-}
