0a7de6170ba87d37d98ef74b247e2436b1dda781
KochubeevAndrei <manualcheg@yandex.ru>
2025-10-02 19:35:18 +0300

    KS-5-2 Year of birth. Calculating age

--- src/main/kotlin/lesson5/lesson5_task2.kt
+++ src/main/kotlin/lesson5/lesson5_task2.kt
@@ -1,5 +1,12 @@
 package org.example.lesson5
 
-fun main() {
+import java.time.Year
 
+const val AGE_ALLOWED = 18
+
+fun main() {
+    println("Введите год вашего рождения:")
+    val yearOfBirth = readln().trim().toInt()
+    val oldEnough = (Year.now().value - yearOfBirth) >= AGE_ALLOWED
+    println(if (oldEnough) "\nПоказать экран со скрытым контентом" else "\nВыйти на главный экран")
 }
