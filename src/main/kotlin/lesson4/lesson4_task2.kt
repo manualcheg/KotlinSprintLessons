2458d5f4309fd6eab13c4f683738421c093c36f8
KochubeevAndrei <manualcheg@yandex.ru>
2025-10-02 10:22:47 +0300

    KS-4-2 checking weight & volume refactoring

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,16 +1,20 @@
 package org.example.lesson4
 
+const val WEIGHT_MIN = 35
+const val WEIGHT_MAX = 100
+const val VOLUME_MAX = 100
+
 fun main() {
     var weight = 20
     var volume = 80
     println(
         "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
-                "${weight > 35 && weight <= 100 && volume < 100}"
+                "${weight > WEIGHT_MIN && weight <= WEIGHT_MAX && volume < VOLUME_MAX}"
     )
     weight = 50
     volume = 100
     println(
         "Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': " +
-                "${weight > 35 && weight <= 100 && volume < 100}"
+                "${weight > WEIGHT_MIN && weight <= WEIGHT_MAX && volume < VOLUME_MAX}"
     )
 }
