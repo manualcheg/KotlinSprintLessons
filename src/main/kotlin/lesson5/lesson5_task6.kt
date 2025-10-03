81404f999a949c2e73a4aca13422b29acf4871a0
KochubeevAndrei <manualcheg@yandex.ru>
2025-10-03 22:37:05 +0300

    KS-5-6 IMT refactor

--- src/main/kotlin/lesson5/lesson5_task6.kt
+++ src/main/kotlin/lesson5/lesson5_task6.kt
@@ -2,25 +2,27 @@ package org.example.lesson5
 
 import kotlin.math.pow
 
+private const val IMT_NOT_ENOUGH_MIN = 0.0
+private const val IMT_NOT_ENOUGH_MAX = 18.5
+private const val IMT_NORMAL_MIN = 18.5
+private const val IMT_NORMAL_MAX = 25.0
+private const val IMT_OVER_MIN = 25.0
+private const val IMT_OVER_MAX = 30.0
+
 fun main() {
-    val weight: Double //Kg
-    val height: Double //centimeters
 
     println("Укажите ваш вес в Кг:")
-    weight = readln().trim().toDouble()
+    val weight: Double = readln().trim().toDouble() //Kg
     println("Укажите ваш рост в см:")
-    height = readln().trim().toDouble()
+    val height: Double = readln().trim().toDouble() //centimeters
 
     val imt = weight / (height / 100).pow(2)
 
-    val resultToPrint = if (imt in 0.0..<18.5) {
-        "ИМТ < 18.5: Недостаточная масса тела"
-    } else if (imt in 18.5..<25.0) {
-        "18.5 ≤ ИМТ < 25: Нормальная масса тела"
-    } else if (imt in 25.0..<30.0) {
-        "25 ≤ ИМТ < 30: Избыточная масса тела"
-    } else {
-        "ИМТ ≥ 30: Ожирение"
+    val resultToPrint = when (imt) {
+        in IMT_NOT_ENOUGH_MIN..<IMT_NOT_ENOUGH_MAX -> "ИМТ < 18.5: Недостаточная масса тела"
+        in IMT_NORMAL_MIN..<IMT_NORMAL_MAX -> "18.5 ≤ ИМТ < 25: Нормальная масса тела"
+        in IMT_OVER_MIN..<IMT_OVER_MAX -> "25 ≤ ИМТ < 30: Избыточная масса тела"
+        else -> "ИМТ ≥ 30: Ожирение"
     }
     println("\nВаш ИМТ = %.2f".format(imt))
     println("\n$resultToPrint")
