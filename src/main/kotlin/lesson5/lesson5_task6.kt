4c749ce6bf272c9a15f0758c02c0d8fa7cf0a519
KochubeevAndrei <manualcheg@yandex.ru>
2025-10-04 23:30:26 +0300

    KS-5-6 IMT refactoring 3

--- src/main/kotlin/lesson5/lesson5_task6.kt
+++ src/main/kotlin/lesson5/lesson5_task6.kt
@@ -2,7 +2,6 @@ package org.example.lesson5
 
 import kotlin.math.pow
 
-private const val IMT_NOT_ENOUGH_MIN = 0.0
 private const val IMT_NOT_ENOUGH_MAX = 18.5
 private const val IMT_NORMAL_MAX = 25.0
 private const val IMT_OVER_MAX = 30.0
@@ -16,10 +15,10 @@ fun main() {
 
     val imt = weight / (height / 100).pow(2)
 
-    val resultToPrint = when (imt) {
-        in IMT_NOT_ENOUGH_MIN..<IMT_NOT_ENOUGH_MAX -> "ИМТ < $IMT_NOT_ENOUGH_MAX: Недостаточная масса тела"
-        in IMT_NOT_ENOUGH_MAX..<IMT_NORMAL_MAX -> "$IMT_NOT_ENOUGH_MAX ≤ ИМТ < $IMT_NORMAL_MAX: Нормальная масса тела"
-        in IMT_NORMAL_MAX..<IMT_OVER_MAX -> "$IMT_NORMAL_MAX ≤ ИМТ < $IMT_OVER_MAX: Избыточная масса тела"
+    val resultToPrint = when {
+        imt < IMT_NOT_ENOUGH_MAX -> "ИМТ < $IMT_NOT_ENOUGH_MAX: Недостаточная масса тела"
+        imt in IMT_NOT_ENOUGH_MAX..<IMT_NORMAL_MAX -> "$IMT_NOT_ENOUGH_MAX ≤ ИМТ < $IMT_NORMAL_MAX: Нормальная масса тела"
+        imt in IMT_NORMAL_MAX..<IMT_OVER_MAX -> "$IMT_NORMAL_MAX ≤ ИМТ < $IMT_OVER_MAX: Избыточная масса тела"
         else -> "ИМТ ≥$IMT_OVER_MAX: Ожирение"
     }
     println("\nВаш ИМТ = %.2f".format(imt))
